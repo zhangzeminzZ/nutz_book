2551813e0637609a6b54add7ae9369eea84fefd8
zhangzemin <zhangzeminzz@163.com>
2020-03-19 14:48:51 +0800

    nutz项目搭建初始化

---
+++ src/main/java/com/zhangzemin/main/MainModule.java
@@ -0,0 +1,21 @@
+package com.zhangzemin.main;
+
+import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.mvc.annotation.*;
+
+/**
+ * @author zhangzemin
+ * @date 2020/3/9 11:04
+ */
+@IocBean
+@Modules(scanPackage = true,packages = {"com.zhangzemin"})
+@Encoding(output = "UTF-8",input = "UTF-8")
+@ChainBy(args = {"conf/config.js"})
+@IocBy(args={"*js", "conf/",
+        "*anno", "com.zhangzemin",
+        "*async",
+        "*tx"
+})
+public class MainModule {
+
+}
